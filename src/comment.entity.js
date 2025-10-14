e1c74e519f02220398aff71593aba2521ce12447
노아진 <you@example.com>
2025-10-14 21:08:14 +0900

    ..

---
+++ src/comment.entity.js
@@ -0,0 +1,95 @@
+"use strict";
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
+    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
+    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Comment = void 0;
+var typeorm_1 = require("typeorm");
+var feed_entity_1 = require("./feed.entity");
+var Comment = function () {
+    var _classDecorators = [(0, typeorm_1.Entity)()];
+    var _classDescriptor;
+    var _classExtraInitializers = [];
+    var _classThis;
+    var _id_decorators;
+    var _id_initializers = [];
+    var _id_extraInitializers = [];
+    var _content_decorators;
+    var _content_initializers = [];
+    var _content_extraInitializers = [];
+    var _feedId_decorators;
+    var _feedId_initializers = [];
+    var _feedId_extraInitializers = [];
+    var _order_decorators;
+    var _order_initializers = [];
+    var _order_extraInitializers = [];
+    var _feed_decorators;
+    var _feed_initializers = [];
+    var _feed_extraInitializers = [];
+    var Comment = _classThis = /** @class */ (function () {
+        function Comment_1() {
+            this.id = __runInitializers(this, _id_initializers, void 0);
+            this.content = (__runInitializers(this, _id_extraInitializers), __runInitializers(this, _content_initializers, void 0));
+            this.feedId = (__runInitializers(this, _content_extraInitializers), __runInitializers(this, _feedId_initializers, void 0));
+            this.order = (__runInitializers(this, _feedId_extraInitializers), __runInitializers(this, _order_initializers, void 0));
+            this.feed = (__runInitializers(this, _order_extraInitializers), __runInitializers(this, _feed_initializers, void 0));
+            __runInitializers(this, _feed_extraInitializers);
+        }
+        return Comment_1;
+    }());
+    __setFunctionName(_classThis, "Comment");
+    (function () {
+        var _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+        _id_decorators = [(0, typeorm_1.PrimaryGeneratedColumn)()];
+        _content_decorators = [(0, typeorm_1.Column)('text')];
+        _feedId_decorators = [(0, typeorm_1.Column)()];
+        _order_decorators = [(0, typeorm_1.Column)()];
+        _feed_decorators = [(0, typeorm_1.ManyToOne)(function () { return feed_entity_1.Feed; }), (0, typeorm_1.JoinColumn)({ name: 'feedId' })];
+        __esDecorate(null, null, _id_decorators, { kind: "field", name: "id", static: false, private: false, access: { has: function (obj) { return "id" in obj; }, get: function (obj) { return obj.id; }, set: function (obj, value) { obj.id = value; } }, metadata: _metadata }, _id_initializers, _id_extraInitializers);
+        __esDecorate(null, null, _content_decorators, { kind: "field", name: "content", static: false, private: false, access: { has: function (obj) { return "content" in obj; }, get: function (obj) { return obj.content; }, set: function (obj, value) { obj.content = value; } }, metadata: _metadata }, _content_initializers, _content_extraInitializers);
+        __esDecorate(null, null, _feedId_decorators, { kind: "field", name: "feedId", static: false, private: false, access: { has: function (obj) { return "feedId" in obj; }, get: function (obj) { return obj.feedId; }, set: function (obj, value) { obj.feedId = value; } }, metadata: _metadata }, _feedId_initializers, _feedId_extraInitializers);
+        __esDecorate(null, null, _order_decorators, { kind: "field", name: "order", static: false, private: false, access: { has: function (obj) { return "order" in obj; }, get: function (obj) { return obj.order; }, set: function (obj, value) { obj.order = value; } }, metadata: _metadata }, _order_initializers, _order_extraInitializers);
+        __esDecorate(null, null, _feed_decorators, { kind: "field", name: "feed", static: false, private: false, access: { has: function (obj) { return "feed" in obj; }, get: function (obj) { return obj.feed; }, set: function (obj, value) { obj.feed = value; } }, metadata: _metadata }, _feed_initializers, _feed_extraInitializers);
+        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
+        Comment = _classThis = _classDescriptor.value;
+        if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        __runInitializers(_classThis, _classExtraInitializers);
+    })();
+    return Comment = _classThis;
+}();
+exports.Comment = Comment;
