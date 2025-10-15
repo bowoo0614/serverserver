a09c99e638d4442f886f535fca4cb5c004767cf8
노아진 <you@example.com>
2025-10-15 20:36:23 +0900

    fix

--- src/comments/comment.module.ts
+++ src/comments/comment.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CommentController } from "./comment.controller";
 import { CommentService } from "./comment.service";
+import { Comment } from "./entities/comment.entity";
 
 @Module({
     imports:[TypeOrmModule.forFeature([Comment])],
